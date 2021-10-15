fcf3f5c13ec98fed988847dd1eae68ac8b508227
Brice-sogilis <brice@sogilis.com>
2021-10-15 11:19:51 +0200

    Implemented interface and test for grids CRUD ops

--- backend/services/grid-storage/mongo-utils.ts
+++ backend/services/grid-storage/mongo-utils.ts
@@ -6,4 +6,48 @@ async function connectGridDatabase(addr : string = '127.0.0.1', port : number = 
     return client.connect();
 }
 
-export {connectGridDatabase}
+function TODO(message : string = "TODO") : Error {
+    return new Error(message);
+}
+
+class GridDAO {
+    name: string;
+    lines: Array<string>;
+    height: number;
+    width : number;
+    constructor(name : string, lines: Array<string>) {
+        this.name = name;
+        this.lines = lines;
+        this.height = lines.length;
+        this.width = (this.height > 0)?lines[0].length:0
+    }
+}
+
+interface GridCRUDRepository {
+    getGridByName(name : string) : GridDAO;
+    listGrids() : Array<GridDAO>;
+    createGrid(grid : GridDAO) : boolean;
+    deleteGridByName(name : string) : boolean;
+
+}
+
+class MongoGridCRUDRepository implements GridCRUDRepository {
+    client : MongoClient
+    constructor(client : MongoClient) {
+        this.client = client;
+    }
+    getGridByName(name: string): GridDAO {
+        throw TODO();
+    }
+    listGrids(): GridDAO[] {
+        throw TODO();
+    }
+    createGrid(grid: GridDAO): boolean {
+        throw TODO();
+    }
+    deleteGridByName(name: string): boolean {
+        throw TODO();
+    }
+}
+
+export {connectGridDatabase, GridDAO, GridCRUDRepository, MongoGridCRUDRepository}
--- backend/services/grid-storage/test/tests.ts
+++ backend/services/grid-storage/test/tests.ts
@@ -4,7 +4,7 @@ const expect = chai.expect;
 chai.use(chai_as_promised);
 chai.should();
 
-import {connectGridDatabase} from '../mongo-utils'
+import {connectGridDatabase, GridCRUDRepository, GridDAO, MongoGridCRUDRepository} from '../mongo-utils'
 const DB_HOSTNAME_TEST : string = process.env.DB_HOSTNAME_TEST || '127.0.0.1'
 const DB_PORT_TEST : number = (process.env.DB_PORT_TEST) ? parseInt(process.env.DB_PORT_TEST) : 27017
 
@@ -43,4 +43,54 @@ describe('Database connection check', () => {
         });
     });
     
+});
+
+describe('Grid CRUD Repository single object tests', function () {
+    const testGrid : GridDAO = new GridDAO("Test",["ABC","DEF","GHI"]);
+    it('Creates a grid without error', function(done) {
+        connectGridDatabase(DB_HOSTNAME_TEST, DB_PORT_TEST).then((clt) => {
+            const repo : GridCRUDRepository = new MongoGridCRUDRepository(clt);
+            expect(repo.createGrid(testGrid)).to.be.true;
+            expect(repo.listGrids()).not.to.be.empty;
+            clt.close();
+            done();
+        });
+    });
+
+    it('Lists empty grids without error', function (done) {
+        connectGridDatabase(DB_HOSTNAME_TEST, DB_PORT_TEST).then((clt) => {
+            const repo : GridCRUDRepository = new MongoGridCRUDRepository(clt);
+            expect(repo.listGrids()).to.be.empty;
+            clt.close();
+            done();
+        });
+    });
+
+    it('Retrieves a correct grid after creation', function (done) {
+        connectGridDatabase(DB_HOSTNAME_TEST, DB_PORT_TEST).then((clt) => {
+            const repo : GridCRUDRepository = new MongoGridCRUDRepository(clt);
+            repo.createGrid(testGrid);
+            const gridBack = repo.getGridByName(testGrid.name);
+            expect(gridBack.name).to.equals(testGrid.name);
+            expect(gridBack.height).to.equals(testGrid.height);
+            expect(gridBack.width).to.equals(testGrid.width);
+            for(var i = 0;i<testGrid.height;i++){
+                expect(gridBack.lines[i]).to.equals(testGrid.lines[i]);
+            }
+            clt.close();
+            done();
+        });
+    });
+
+    it('Deletes a grid after creation', function(done) {
+        connectGridDatabase(DB_HOSTNAME_TEST, DB_PORT_TEST).then((clt) => {
+            const repo : GridCRUDRepository = new MongoGridCRUDRepository(clt);
+            repo.createGrid(testGrid);
+            expect(repo.listGrids()).not.to.be.empty;
+            expect(repo.deleteGridByName(testGrid.name)).to.be.true;
+            expect(repo.listGrids()).to.be.empty;
+            clt.close();
+            done();
+        });
+    });
 });
