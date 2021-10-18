9f4f0a52dd0e02ceb4b351650cb537bbd8c14260
Brice-sogilis <brice@sogilis.com>
2021-10-18 10:32:39 +0200

    Implemented routes and corrected db access to pass tests

--- backend/services/grid-storage/service.ts
+++ backend/services/grid-storage/service.ts
@@ -2,12 +2,12 @@ import http from 'http';
 import express from 'express';
 import {MongoGridCRUDRepository, connectGridDatabase} from './mongo-utils';
 
-const DB_HOSTNAME = process.env.DB_HOSTNAME
-const DB_PORT = (process.env.DB_PORT) ? parseInt(process.env.DB_PORT) : 27017
-const mongoClient = connectGridDatabase(DB_HOSTNAME, DB_PORT);
-var repository : MongoGridCRUDRepository | null = null;
+let DB_HOSTNAME = process.env.DB_HOSTNAME
+let DB_PORT = (process.env.DB_PORT) ? parseInt(process.env.DB_PORT) : 27017
+let mongoClient = connectGridDatabase(DB_HOSTNAME, DB_PORT);
+let repository : MongoGridCRUDRepository | null = null;
 
-var app = express();
+let app = express();
 app.use(express.json());
 
 async function getRepository() : Promise<MongoGridCRUDRepository> {
@@ -26,4 +26,19 @@ app.get("/grid", async function(req, res) {
     res.status(200).send(await (await getRepository()).listGrids());
 });
 
-http.createServer(app).listen(8888);
+app.get("/grid/:name", async function (req, res) {
+    res.status(200).send(await(await getRepository()).getGridByName(req.params.name));
+});
+
+app.delete("/grid/:name", async function name(req, res) {
+    const present = await (await getRepository()).deleteGridByName(req.params.name);
+    res.sendStatus((present)?200:404);
+});
+
+app.post("/grid", async function(req, res) {
+    await (await getRepository()).createGrid(req.body);
+    res.sendStatus(201);
+});
+
+app.listen(8888);
+export {app}
